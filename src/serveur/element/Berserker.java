f0de860c45b18464adbe1d2c1d8e6f78a92be06f
hydis <hydis@hydis-HP-Pavilion-dm1-Notebook-PC>
2016-01-06 14:19:14 +0100

    Ajout Stratégies

--- src/serveur/element/Berserker.java
+++ src/serveur/element/Berserker.java
@@ -7,11 +7,4 @@ public class Berserker extends Personnage {
 	public Berserker(String nom, String groupe, HashMap<Caracteristique, Integer> carac) {
 		super(nom, groupe, carac);
 	}
-	
-	public void gainFo() {
-		if(this.getCaract(Caracteristique.VIE) < 100) {
-			int gain = (100 - getCaract(Caracteristique.VIE)) / 2;
-			this.incrementeCaract(Caracteristique.FORCE, gain);
-		}
-	}
 }
